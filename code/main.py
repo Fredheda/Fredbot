eb514ab69944da209cc84f846f0da8e0c8729ca3
Fredheda <frederik.heda@gmx.net>
2024-09-16 00:28:38 +0100

    implement openai

--- code/main.py
+++ code/main.py
@@ -1,15 +1,18 @@
-from llm.phi3 import phi3_llm
+from llm.openai_llm import openai_llm
 from tfl_api import construct_tfl_status
 from datetime import datetime
 
 tfl_status = construct_tfl_status()
 
 model_id = "microsoft/Phi-3.5-mini-instruct"
-llm = phi3_llm(model_id)
+llm = openai_llm('gpt4o-mini')
 
-prompt_path = 'prompt_templates/tfl_disruption.txt'
 user_message = 'Is the district line running a good service?'
-prompt = llm.construct_prompt('How are you?')
+
+current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+prompt = llm.construct_prompt(user_message, 'prompt_templates/tfl_disruption.txt', tfl_status=tfl_status, current_datetime=current_datetime)
+
+#prompt = llm.construct_prompt('How are you?')
 
 gen = llm.generate_response(prompt, stream=True)
 for chunk in gen:
